e1146381a12b8da0f98dbfa53e56e5a0f65a9d6d
franck sean <seandjissou@gmail.com>
2024-03-07 21:23:57 +0100

    update user router

--- Servers/Node_server/routes/usersRouter.js
+++ Servers/Node_server/routes/usersRouter.js
@@ -3,34 +3,22 @@ const router = express.Router();
 
 const userRouter = (connection) => {
   router.post("/connexion", async (req, res) => {
-    try {
       const { email, password } = req.body;
-
-      if (!email || !password) {
-        return res.status(400).send("Email and password are required");
-      }
 
       const user = await new Promise((resolve, reject) => {
         connection.query('SELECT * FROM users WHERE email = ?', [email], (error, results) => {
           if (error) {
             reject(error);
           } else {
+            console.log(`results : ${results}`)
             resolve(results);
           }
         });
       });
-
-      if (user.length > 0) {
-        console.log(user[0]);
-        res.status(200).json(user[0]);
-      } else {
-        res.status(404).send("User not found");
-      }
-    } catch (error) {
-      console.error("Error querying database:", error);
-      res.status(500).send("Internal server error");
+      console.log(user);
+      res.status(200).json(user[0]);
     }
-  });
+);
 
   return router;
 };
