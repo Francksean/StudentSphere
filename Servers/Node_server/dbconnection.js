ef0c8831041fefa243ef07a41298c6adbd73c78b
franck sean <seandjissou@gmail.com>
2024-03-08 14:05:58 +0100

    mcd et dbconnection est inactif

--- Servers/Node_server/dbconnection.js
+++ Servers/Node_server/dbconnection.js
@@ -1,23 +1,23 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-// initialisation des paramètres de connexion à la base de données
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: "studentspherebdd"
-});
+// // initialisation des paramètres de connexion à la base de données
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: "studentspherebdd"
+// });
 
-// fonction de connexion à la bd
-const initConnection = () =>  {
-  console.log('ok server')
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("\n\n\n\nConnected to the database!");
- });
-}
+// // fonction de connexion à la bd
+// const initConnection = () =>  {
+//   console.log('ok server')
+//   connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("\n\n\n\nConnected to the database!");
+//  });
+// }
 
-initConnection()
+// initConnection()
 
-module.exports = initConnection;
-module.exports = connection;
+// module.exports = initConnection;
+// module.exports = connection;
