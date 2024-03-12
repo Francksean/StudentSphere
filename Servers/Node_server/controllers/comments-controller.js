68ba7055c1414a8d4f20189ef272499b4045f438
Jozman2004 <joseph.nkoulou@2027.ucac-icam.com>
2024-03-12 14:37:54 +0100

    Appel de la procédure stockée DeleteComment

--- Servers/Node_server/controllers/comments-controller.js
+++ Servers/Node_server/controllers/comments-controller.js
@@ -6,14 +6,15 @@ exports.addComment = (req, res) => {
 
   const connection = dbconnector.createConnection();
   dbconnector.initConnection(connection, () => {
+    const currentDate = date();
     const query = `CALL InsertComment(${authorId}, ${relativeId}, '${content}', '${date}')`;
 
     connection.query(query, (error, results) => {
       if (error) {
         console.error(`Error: ${error}`);
-        res.status(500).json({ message: "An error occurred while inserting the comment", success: false, error: error });
+        res.status(500).json({ message: "Une erreur s'est produite lors de l'ajout du commentaire", success: false, error: error });
       } else {
-        res.status(200).json({ message: "Comment inserted successfully", success: true, results: results });
+        res.status(200).json({ message: "Commentaire ajouté avec succès", success: true, results: results });
       }
     });
   });
@@ -25,32 +26,32 @@ exports.showComments = (req, res) => {
 
   const connection = dbconnector.createConnection();
   dbconnector.initConnection(() => {
-    connection.query('CALL ShowComments(?, ?)', [table, id], (err, results) => {
-      if (err) {
-        console.error('Error executing stored procedure:', err);
-        res.status(500).json({ error: 'An error occurred while fetching comments.' });
+    const currentDate = date();
+    connection.query('CALL ShowComments(?, ?)', [table, id], (error, results) => {
+      if (error) {
+        console.log(`${error}`);
+        res.send({ message: "Une erreur s'est produite lors de la récupération des commentaires", success: false, error: error });
       } else {
-        res.json(results[0]);
+        res.send({ message: "Commentaires récupérés avec succès", success: true, results: results });
       }
     });
   });
 };
 
 exports.deleteComment = (req, res) => {
-
-  const { id } = req.params
+  const { id } = req.params;
   const { table } = req.body;
 
-  const connection = dbconnector.createConnection()
-  dbconnector.initConnection
-
-  const deletedComment = connection.query(`DELETE FROM ${table} WHERE id = '${id}'`, 
-  (error, results) => {
-    if(error){
-      console.log(`${error}`)
-      res.send({ message : "une erreur s'est produite lors de la suppression", success : false, error : error })
-    }else{
-      res.send({ message : "commentaire supprimé avec succès", success : true, results: results})
-    }
-  })
-}
+  const connection = dbconnector.createConnection();
+  dbconnector.initConnection(() => {
+    const currentDate = date();
+    connection.query('CALL DeleteComment(?, ?)', [table, id], (error, results) => {
+      if (error) {
+        console.log(`${error}`);
+        res.send({ message: "Une erreur s'est produite lors de la suppression du commentaire", success: false, error: error });
+      } else {
+        res.send({ message: "Commentaire supprimé avec succès", success: true, results: results });
+      }
+    });
+  });
+};
