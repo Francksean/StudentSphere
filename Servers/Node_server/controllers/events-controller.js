7d7666372cb0feb861ae5374457502c4d282727a
Jozman2004 <joseph.nkoulou@2027.ucac-icam.com>
2024-03-12 18:45:43 +0100

    Appel de la procédure stockée InsertEvent

--- Servers/Node_server/controllers/events-controller.js
+++ Servers/Node_server/controllers/events-controller.js
@@ -4,76 +4,76 @@ const dbconnector = require('../utils/dbconnector')
 
 
 
-exports.addEvent = (req, res)=>{
-  
-  const { authorId, eventName, eventDescription, poster, category  } = req.body;
+exports.addEvent = (req, res) => {
+  const { authorId, name, description, poster, category } = req.body;
 
-  const connection = dbconnector.createConnection()
-  dbconnector.initConnection
-
-  const newEvent = connection.query(
-    `INSERT INTO events (authorId, name, description, datePosted, poster, category, state) 
-    VALUES (${authorId}, "${eventName}", "${eventDescription}", '${date}', '${poster}', '${category}', 'proposed')`,
-    (error, results) => {
-      if (error) {
-        res.status(500).json({ message: "An error occurred while registering the event" });
-      } else {
-        res.status(200).json({ message: "event proposed successfully", success : true, results: results });
+  const connection = dbconnector.createConnection();
+  dbconnector.initConnection(connection, () => {
+    const newEvent = connection.query(
+      `CALL InsertEvent(${authorId}, "${name}", "${description}", "${poster}", "${category}")`,
+      (error, results) => {
+        if (error) {
+          res.status(500).json({ message: 'Un problème est survenu' });
+        } else {
+          res.status(200).json({ message: 'Event proposed successfully', success: true, results: results });
+        }
       }
-    }
-  );
-
-}
+    );
+  });
+};
 
 exports.validateEvent = (req, res) => {
-
-  const { id } = req.params
+  const { id } = req.params;
   const { beginDate, endDate } = req.body;
-  
-  const connection = dbconnector.createConnection()
-  dbconnector.initConnection
 
-  const validateEvent = connection.query(
-    `UPDATE events SET beginDate = '${beginDate}',endDate = '${endDate}', state = 'scheduled' WHERE id = '${id}'`,
-    (error, results) => {
-      if (error) {
-        console.error(`Error: ${error}`);
-        res.status(500).json({ message: "An error occurred while validating the event" });
-      } else {
-        res.status(200).json({ message: "event validate successfully", success : true, results: results });
+  const connection = dbconnector.createConnection();
+  dbconnector.initConnection(connection, () => {
+    const validateEvent = connection.query(
+      `CALL ValidateEvent(${id}, "${beginDate}", "${endDate}")`,
+      (error, results) => {
+        if (error) {
+          console.error(`Error: ${error}`);
+          res.status(500).json({ message: 'An error occurred while validating the event' });
+        } else {
+          res.status(200).json({ message: 'Event validated successfully', success: true, results: results });
+        }
       }
-    }
-  )
-
-}
+    );
+  });
+};
 
 exports.getAllPastEvents = (req, res) => {
-  
-  const connection = dbconnector.createConnection()
-  dbconnector.initConnection
-
-  connection.query(
-    `SELECT * from events WHERE endDate < '2024-03-09'`,
-    (error, results) => {
-      if (error) {
-        console.error(`Error: ${error}`);
-        res.status(500).json({ message: "An error occurred while fetching past events", success : false });
-      } else {
-        res.status(200).json({ message: "past events are available", success : true, results: results });
+  const connection = dbconnector.createConnection();
+  dbconnector.initConnection(connection, () => {
+    connection.query(
+      'CALL GetAllPastEvents()',
+      (error, results) => {
+        if (error) {
+          console.error(`Error: ${error}`);
+          res.status(500).json({ message: 'An error occurred while fetching past events', success: false });
+        } else {
+          res.status(200).json({ message: 'Past events are available', success: true, results: results });
+        }
       }
-    }
-  );
-}
+    );
+  });
+};
 
 
 exports.deleteEvent = (req, res) => {
-  const connection = dbconnector.createConnection()
-  dbconnector.initConnection
-
-  const { id } = req.params
-  
-  const deletedEvent = connection.query(
-    `DELETE FROM events WHERE id = '${id}'`
-  )
-
-}
+  const connection = dbconnector.createConnection();
+  dbconnector.initConnection(connection, () => {
+    const { id } = req.params;
+    const deletedEvent = connection.query(
+      `CALL DeleteEvent(${id})`,
+      (error, results) => {
+        if (error) {
+          console.error(`Error: ${error}`);
+          res.status(500).json({ message: 'An error occurred while deleting the event' });
+        } else {
+          res.status(200).json({ message: 'Event deleted successfully' });
+        }
+      }
+    );
+  });
+};
