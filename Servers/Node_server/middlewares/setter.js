4d1e53035a37b14cfe1117f55e9fe87f35f55422
franck sean <seandjissou@gmail.com>
2024-03-13 02:32:27 +0100

    ajout du middleware pour parametrer les headers des requêtes depuis le front

---
+++ Servers/Node_server/middlewares/setter.js
@@ -0,0 +1,10 @@
+//pour paramétrer les headers des requêtes
+
+exports.setReqHeader = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  next()
+}
