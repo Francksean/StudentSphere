d158049854bd368d7deec089499db4f41337010f
franck sean <seandjissou@gmail.com>
2024-03-13 01:53:25 +0100

    attempting server-client connetion

---
+++ Client/events programmés/eventsForm/addEvent.js
@@ -0,0 +1,92 @@
+console.log("test test")
+
+// on récupère le statut de l'utilisateur
+
+// const userStatus = localStorage.getItem('status') --Djissou
+const userStatus = 1;
+const eventDateElems = []
+
+//récuperer les éléments à affichage conditionnel --Djissou
+document.addEventListener("DOMContentLoaded", function() {
+  const eventDateElems = Array.from(document.querySelectorAll(".input_date"));
+  console.log(eventDateElems);
+  
+  const userStatus = 1;
+
+  switch(userStatus){
+    case 0 :
+      eventDateElems.forEach((item) => {
+        item.hidden = false;
+      });
+    break;
+    case 1 :true
+      eventDateElems.forEach((item) => {
+        item.hidden = false;
+      });
+    break;
+    case 2 :
+      eventDateElems.forEach((item) => {
+        item.hidden = false;
+      });
+    break;
+  }
+});
+
+
+
+function addEvent() {
+  //récupérer les valeurs des champs
+
+  // let eventName = document.getElementById("event-name").value;
+  // let eventDescription = document.getElementById("event-description").value;
+  // let eventPoster = document.getElementById("event-poster").value;
+  // let eventBeginDate = document.getElementById("event-begin-date").value;
+  // let eventEndDate = document.getElementById("event-end-date").value;
+  // let eventCategory = document.getElementById("event-category").value;
+
+  const eventData = {
+    // authorId : localStorage.getItem('userId'),
+    authorId : 2,
+    eventName: "name factice",
+    eventDescription: "descriprtion de test de la fonctionnalité depuis le front",
+    // poster : eventPoster,
+    poster : "image1.jpg",
+    category : "activités"
+  };
+
+  if(userStatus == 1){
+    eventData["beginDate"] = eventBeginDate
+    eventData["endDate"] = eventEndDate
+  }
+  // on contacte la route API pour ajouter un évènement
+  (async () => {
+    const sendDatas = await fetch('http://localhost:3000/events/add', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(eventData)
+    });
+    const response = await sendDatas.json();
+  
+    console.log(response);
+  })();
+
+  // Réinitialiser les champs de saisie
+  eventName = "";
+  eventDescription = ""
+  eventCategory = ""
+  eventPoster = ""
+  eventBeginDate  = ""
+  eventEndDate = ""
+  
+  // Afficher un message de succès ou effectuer d'autres actions nécessaires
+
+  // Mettre le focus sur le champ de saisie du nom de l'événement
+  document.getElementById("event-name").focus();
+}
+
+// Ajouter un écouteur d'événement au bouton "Ajouter l'événement"
+var addButton = document.getElementById("add-event-button");
+addButton.addEventListener("click", addEvent);
--- Servers/Node_server/middlewares/auth.js
+++ Servers/Node_server/middlewares/auth.js
@@ -1,13 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 exports.verifyToken = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ error: 'Accès refusé' });
-  try {
-    const decoded = jwt.verify(token, 'your-secret-key');
-    req.userId = decoded.userId;
-    next();
-  } catch (error) {
-    res.status(401).json({ error: 'token invalid' });
-  }
+
+  console.log("authentification passée")
+  res.send("authentification passée")
+  next()
+  // const token = req.header('Authorization');
+  // if (!token) return res.status(401).json({ error: 'Accès refusé' });
+  // try {
+  //   const decoded = jwt.verify(token, 'your-secret-key');
+  //   req.userId = decoded.userId;
+  //   next();
+  // } catch (error) {
+  //   res.status(401).json({ error: 'token invalid' });
+  // }
 };
