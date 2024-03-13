d8bd9e9ecdba255d6a08e355da23914c48dd9f6e
Jozman2004 <joseph.nkoulou@2027.ucac-icam.com>
2024-03-13 02:02:31 +0100

    Appel getAllProducts

--- Servers/Node_server/controllers/products-controller.js
+++ Servers/Node_server/controllers/products-controller.js
@@ -2,18 +2,19 @@ const dbconnector = require('../utils/dbconnector')
 
 
 exports.getAllProducts = (req, res) => {
-  const connection = dbconnector.createConnection()
-  dbconnector.initConnection
-
-  const feed = connection.query('SELECT * FROM products', 
-  (error, results) => {
-    if(error){
-      res.json({ success : false, message : "Problème lors du chargement des produits ",error : error });
-    }else{
-      res.json({ success : true, results : results });
-    }
+  const connection = dbconnector.createConnection();
+  dbconnector.initConnection(connection, () => {
+    const query = 'CALL GetAllProducts()';
+    connection.query(query, (error, results) => {
+      if (error) {
+        res.status(500).json({ success: false, message: 'Problème lors du chargement des produits', error: error });
+      } else {
+        res.status(200).json({ success: true, results: results[0] });
+      }
+      dbconnector.endConnection(connection);
+    });
   });
-}
+};
 
 exports.getProductsByCategory = (req, res) => {
 
