14c0420dae4101823ce560fbe58907e781aaa56f
Jozman2004 <joseph.nkoulou@2027.ucac-icam.com>
2024-03-14 01:39:21 +0100

    Multer fonctionnel

---
+++ Servers/Node_server/middlewares/Events-multer-config.js
@@ -0,0 +1,22 @@
+const multer = require ('multer');
+
+const MIME_TYPES = {
+    'image/jpg' : 'jpg',
+    'image/jpeg' : 'jpg',
+    'image/png' : 'png'
+};
+
+const storage = multer.diskStorage ({
+    destination : (req, file, callback) => {
+        console.log('ok multer')
+        callback (null, './ImagesEvents') 
+    },
+    filename : (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIME_TYPES [file.mimetype];
+        callback (null, name + Date.now() + '.' + extension);
+        console.log("fin multer")
+    }
+});
+
+module.exports = multer({storage: storage}).single('Image');
---
+++ Servers/Node_server/routes/image-Event-router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const multer = require('../middlewares/Events-multer-config');
+
+const app = express();
+const upload = multer({ dest: '../ImagesEvents' }).single('Image');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('upload');
+});
+
+app.post('/ImagesEvents', (req, res) => {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            console.log('Une erreur Multer s\'est produite :', err);
+        } else if (err) {
+            console.log('Une erreur s\'est produite lors de l\'upload :', err);
+        } else {
+            console.log('L\'upload s\'est terminé avec succès');
+        }
+        res.send('Upload terminé');
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Le serveur écoute sur le port 3000');
+});
