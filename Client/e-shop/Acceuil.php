0ca4be81f41e15efe40fab1c7f3e467a79988b19
Erika049 <147406070+Erika049@users.noreply.github.com>
2024-03-18 05:50:44 +0000

    Add files via upload

--- Client/e-shop/Acceuil.php
+++ Client/e-shop/Acceuil.php
@@ -5,12 +5,12 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <title>BDE Shop</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
    <section class="flex-around">
-      <img src="Images/logo.jpg" alt=" logo">
+      <img src="Images/logo" alt=" logo">
       <h1 class="logo-titre">
         Shop with <br />
         StudentSphere
@@ -25,33 +25,31 @@
         <a href="Connexion.php" class="fas fa-user"></a>
      </div>
     </nav>
+
     <div class="content">
       <img src="Images/shop.png" alt="hummm" />
-    </div>
+    </div> 
 
-<section class="icons-container">
-  <h1>PARCOUREZ LES DIFFERENTES CATEGORIE DE PRODUITS</h1>
-
-<div class="icons">
-    <a href="fruits.php" class="bouton">FRUITS</a>
-</div>
-
-<div class="icons">
-    <a href="dej.php" class="bouton">DEJEUNER</a>
-</div>
-
-<div class="icons">
-    <a href="goodies.php" class="bouton">GOODIES</a>
-</div>
-
-<div class="icons">
-    <a href="aliment.php" class="bouton">ALIMENTATION</a>
-</div>
-<div class="icons">
-    <a href="hygiene.php" class="bouton">HYGIENE</a>
-</div>
-
-</section>
-
+    <h1>PARCOUREZ LA BOUTIQUE ET COMMANDER DES PRODUITS AUX CHOIX</h1>
+   <!--<div class="icons-container">
+        <div class="icons">
+            <a href="fruits.php" class="bouton">FRUITS</a>
+        </div>
+    
+        <div class="icons">
+            <a href="dej.php" class="bouton">DEJEUNER</a>
+        </div>
+    
+        <div class="icons">
+            <a href="goodies.php" class="bouton">GOODIES</a>
+        </div>
+    
+        <div class="icons">
+            <a href="aliment.php" class="bouton">ALIMENTATION</a>
+        </div>
+        <div class="icons">
+            <a href="hygiene.php" class="bouton">HYGIENE</a>
+        </div>
+    </div>-->
 </body>
 </html>
